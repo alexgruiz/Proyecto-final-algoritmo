1b57176a0a8eb97ca2190b40e18a3bed6f8d8041
Pierojjd <131670788+Pierojjd@users.noreply.github.com>
2025-08-07 11:34:50 -0400

    Update Data_manager.py

--- Data_manager.py
+++ Data_manager.py
@@ -1,1 +1,19 @@
+import requests 
+from models import Departamento, Artista, Obra
 
+def obtener departamentos ():
+  ''Obtiene la lista de departamentos del museo desde la API''
+  url = ''https://collectionapi.metmuseum.org/public/collection/v1/departments''
+  response = requests.get(url)
+  data = response.json ()
+
+  departamentos = []
+  for dept in data['departments']:
+      departamentos.append(Departamento(dept['departmentId'], dept['displayName']))
+
+  return departamentos
+
+def 
+
+
+    
