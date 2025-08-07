af57efcf3420dbe1f37bc6e3c5f34ba3d1f789c3
alexgruiz <131670942+alexgruiz@users.noreply.github.com>
2025-08-06 23:55:11 -0400

    Update Models.py

---
+++ Models.py
@@ -0,0 +1,25 @@
+class Departamento:
+    def __init__(self, id, nombre):
+        self.id = id
+        self.nombre = nombre
+    def __str__(self):
+        return f"{self.id}: {self.nombre}"
+class Artista:
+    def __init__(self, nombre, nacionalidad, fecha_nacimiento, fecha_muerte):
+        self.nombre = nombre
+        self.nacionalidad = nacionalidad
+        self.fecha_nacimiento = fecha_nacimiento
+        self.fecha_muerte = fecha_muerte      
+    def __str__(self):
+        return f"{self.nombre} ({self.nacionalidad})"
+class Obra:
+    def __init__(self, id, titulo, artista, departamento, tipo, fecha_creacion, imagen_url):
+        self.id = id
+        self.titulo = titulo
+        self.artista = artista
+        self.departamento = departamento
+        self.tipo = tipo
+        self.fecha_creacion = fecha_creacion
+        self.imagen_url = imagen_url      
+    def __str__(self):
+        return f"{self.id}: {self.titulo} - {self.artista.nombre}"
